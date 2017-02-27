74d47d060a2b6f5597d95db98e3e1af2c5476072
GeorgeAnson <georgeanson@foxmail.com>
2017-02-27 19:36:16 +0800

    emule翻墙软件源码项目

---
+++ emule-gifc/trunk/srchybrid/lang/zh_TW.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by zh_TW.rc
+//
+#define IDS_GIFC1                       1901
+#define IDS_OPENGIFC                    1903
+#define IDS_SHOWLESS                    1910
+#define IDS_SHOWMORE                    1911
+#define IDS_SHOWEXT                     1912
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
